f400937a30632f017dfef2b977423a3c7af6e819
Panni Belevski <pannibel@gmail.com>
2022-09-15 19:50:33 +0200

    new array not working yet

---
+++ datacleanup.js
@@ -0,0 +1,172 @@
+"use strict"
+
+let studentJSON;
+
+let fullname;
+let house;
+let gender;
+
+let firstname;
+let middlename;
+let lastname;
+let nickname;
+let image;
+let firstletter;
+
+let newStudentArray;
+
+// FETCHING YAY
+
+document.addEventListener("DOMContentLoaded", event => {
+    init();
+});
+
+async function init() {
+    await loadData();
+    console.log("init");
+
+    prepareData();
+}
+
+async function loadData() {
+    const response = await fetch("https://petlatkea.dk/2021/hogwarts/students.json");
+    studentJSON = await response.json();
+    console.log(studentJSON)
+}
+
+
+function prepareData() {
+    console.log("prepareData");
+
+// FOR EACH OBJECT IN THE JSON DATA:
+// read the properties of the JSON object
+    studentJSON.forEach(student => {
+
+        fullname = student.fullname;
+        gender = student.gender;
+        house = student.house;
+
+// GENERAL CHANGES
+        // cleaning extra whitespaces
+            fullname = fullname.replace(/\s+/g, ' ').trim();
+            house = house.replace(/\s+/g, ' ').trim();
+
+        // making all letters lowercase
+            fullname = fullname.toLowerCase();
+            house = house.toLowerCase();
+
+        // capitalize first letters
+            firstletter = fullname[0].toUpperCase();
+            fullname = fullname.replace(fullname[0], firstletter);
+
+            firstletter = house[0].toUpperCase();
+            house = house.replace(house[0], firstletter);
+
+        // capitalize first letter after " "
+            let fullnameArr = fullname.split(" ");
+
+            for (let i = 0; i < fullnameArr.length; i++) {
+                fullnameArr[i] = fullnameArr[i][0].toUpperCase() + fullnameArr[i].substring(1);
+            }
+
+            fullname = fullnameArr.join(" ");
+
+        // capitalize first letter after "-"
+            let fullnameArr2 = fullname.split("-");
+
+            for (let i = 0; i < fullnameArr2.length; i++) {
+                fullnameArr2[i] = fullnameArr2[i][0].toUpperCase() + fullnameArr2[i].substring(1);
+            }
+
+            fullname = fullnameArr2.join("-");
+
+        // capitalize first letter after "
+            let fullnameArr3 = fullname.split(`"`);
+
+            for (let i = 0; i < fullnameArr3.length; i++) {
+                fullnameArr3[i] = fullnameArr3[i][0].toUpperCase() + fullnameArr3[i].substring(1);
+            }
+
+            fullname = fullnameArr3.join(`"`);
+
+
+// CONVERTING THEM INTO VARIABLES WE WANT
+        // making first names
+        if (fullname.includes(" ")) {
+            firstname = fullname.substring(fullname[0], fullname.indexOf(" "));
+        } else {
+            firstname = fullname.substring(0);
+        }
+
+        // making middle names (if they have it)
+        if (fullname.includes(" ") && fullname.lastIndexOf(" ") != fullname.indexOf(" ") && !fullname.includes(`"`)) {
+            middlename = fullname.substring(fullname.indexOf(" ")+1, fullname.lastIndexOf(" "));
+        } else {
+            middlename = ""
+        }
+
+        // making last names (if they have it)
+        // hyphen cases
+        if (fullname.includes(" ")) {
+            lastname = fullname.substring(fullname.lastIndexOf(" ")+1);
+        } else {
+            lastname = "";
+        }
+
+        // making nicknames (if they have it)
+        if (fullname.includes(`"`)) {
+            nickname = fullname.substring(fullname.indexOf("\"")+1, fullname.lastIndexOf("\""));
+        } else {
+            nickname = "";
+        }
+
+        //adding images
+        if (lastname && !lastname.includes("-")) {
+            image = lastname.toLowerCase() + "_" + firstname[0].toLowerCase() + ".png";
+        } else if (lastname.includes("-")) {
+            let afterhyphen = lastname.substring(lastname.indexOf("-")+1);
+            image = afterhyphen.toLowerCase() + "_" + firstname[0].toLowerCase() + ".png";
+        }
+
+        console.log("first name:", firstname);
+        console.log("middle name:", middlename);
+        console.log("last name:", lastname);
+        console.log("nickname:", nickname);
+        console.log("house:", house);
+        console.log("gender:", gender);
+        console.log("image:", image)
+        console.log("");
+
+        });
+
+
+    // BUILDING A NEW OBJECT PROTOTYPE
+
+    const Student = {
+        firstname: "",
+        middlename: "",
+        lastname: "",
+        nickname: "",
+        gender: "",
+        house: "",
+        image: "",
+    }
+
+    // create a new object from prototype
+    const student = Object.create(Student);
+
+    // set properties on the object to the variables
+    student.firstname = firstname;
+    student.middlename = middlename;
+    student.lastname = lastname;
+    student.nickname = nickname;
+    student.gender = gender;
+    student.house = house;
+
+    // add the object to an array of data objects
+    newStudentArray = Array.from(student);
+
+    console.log(newStudentArray)
+
+
+}
--- script.js
+++ script.js
@@ -1,160 +1,151 @@
-"use strict"
+"use strict";
 
-let studentJSON;
+// comment index
+// !
+// *
+// ?
+// TODO
 
-let fullname;
-let house;
-let gender;
+// * STAGE: DISPLAYING THE LIST OF STUDENTS, WITH SORTING, FILTERING, SEARCHING, AND POP-UP
 
-let firstname;
-let middlename;
-let lastname;
-let nickname;
-let image;
-let firstletter;
+// LETS START THIS S
 
-let newStudentArray;
+// GLOBAL OBJECTS AND VARIABLES
+let fullname, house, gender, firstname, middlename, lastname, nickname, image, firstletter;
+let allStudents = [];
+
+// The prototype for all students: 
+const Student = {
+    firstname: "",
+    middlename: "",
+    lastname: "",
+    nickname: "",
+    gender: "",
+    house: "",
+    image: "",
+};
 
 // FETCHING YAY
-
-document.addEventListener("DOMContentLoaded", event => {
-    init();
+window.addEventListener("DOMContentLoaded", event => {
+    start();
 });
 
-async function init() {
-    await loadData();
-    console.log("init");
+function start() {
+    console.log("start");
 
-    prepareData();
+    // ! later
+    //registerButtons();
+    loadData();
 }
 
 async function loadData() {
     const response = await fetch("https://petlatkea.dk/2021/hogwarts/students.json");
-    studentJSON = await response.json();
-    console.log(studentJSON)
+    const studentJSON = await response.json();
+
+    // when loaded, prepare data objects
+    prepareObjects(studentJSON);
+    //console.log(studentJSON);
 }
 
+function prepareObjects(jsonData) {
+    console.log("prepareObjects");
 
-function prepareData() {
-    console.log("prepareData");
+    allStudents = jsonData.map(prepareObject);
+    displayList(allStudents);
+}
 
-// FOR EACH OBJECT IN THE JSON DATA:
-// read the properties of the JSON object
-    studentJSON.forEach(student => {
+function prepareObject(jsonObject) {
+    const student = Object.create(Student);
 
-        fullname = student.fullname;
-        gender = student.gender;
-        house = student.house;
+    fullname = jsonObject.fullname;
+    gender = jsonObject.gender;
+    house = jsonObject.house;
 
 // GENERAL CHANGES
-        // cleaning extra whitespaces
-            fullname = fullname.replace(/\s+/g, ' ').trim();
-            house = house.replace(/\s+/g, ' ').trim();
+    // cleaning extra whitespaces
+        fullname = fullname.replace(/\s+/g, ' ').trim();
+        house = house.replace(/\s+/g, ' ').trim();
 
-        // making all letters lowercase
-            fullname = fullname.toLowerCase();
-            house = house.toLowerCase();
+    // making all letters lowercase
+        fullname = fullname.toLowerCase();
+        house = house.toLowerCase();
 
-        // capitalize first letters
-            firstletter = fullname[0].toUpperCase();
-            fullname = fullname.replace(fullname[0], firstletter);
+    // capitalize first letters
+        firstletter = fullname[0].toUpperCase();
+        fullname = fullname.replace(fullname[0], firstletter);
 
-            firstletter = house[0].toUpperCase();
-            house = house.replace(house[0], firstletter);
+        firstletter = house[0].toUpperCase();
+        house = house.replace(house[0], firstletter);
 
-        // capitalize first letter after " "
-            let fullnameArr = fullname.split(" ");
+    // capitalize first letter after " "
+        let fullnameArr = fullname.split(" ");
 
-            for (let i = 0; i < fullnameArr.length; i++) {
-                fullnameArr[i] = fullnameArr[i][0].toUpperCase() + fullnameArr[i].substring(1);
-            }
+        for (let i = 0; i < fullnameArr.length; i++) {
+            fullnameArr[i] = fullnameArr[i][0].toUpperCase() + fullnameArr[i].substring(1);
+        }
 
-            fullname = fullnameArr.join(" ");
+        fullname = fullnameArr.join(" ");
 
-        // capitalize first letter after "-"
-            let fullnameArr2 = fullname.split("-");
+    // capitalize first letter after "-"
+        let fullnameArr2 = fullname.split("-");
 
-            for (let i = 0; i < fullnameArr2.length; i++) {
-                fullnameArr2[i] = fullnameArr2[i][0].toUpperCase() + fullnameArr2[i].substring(1);
-            }
+        for (let i = 0; i < fullnameArr2.length; i++) {
+            fullnameArr2[i] = fullnameArr2[i][0].toUpperCase() + fullnameArr2[i].substring(1);
+        }
 
-            fullname = fullnameArr2.join("-");
+        fullname = fullnameArr2.join("-");
 
-        // capitalize first letter after "
-            let fullnameArr3 = fullname.split(`"`);
+    // capitalize first letter after "
+        let fullnameArr3 = fullname.split(`"`);
 
-            for (let i = 0; i < fullnameArr3.length; i++) {
-                fullnameArr3[i] = fullnameArr3[i][0].toUpperCase() + fullnameArr3[i].substring(1);
-            }
+        for (let i = 0; i < fullnameArr3.length; i++) {
+            fullnameArr3[i] = fullnameArr3[i][0].toUpperCase() + fullnameArr3[i].substring(1);
+        }
 
-            fullname = fullnameArr3.join(`"`);
+        fullname = fullnameArr3.join(`"`);
 
 
 // CONVERTING THEM INTO VARIABLES WE WANT
-        // making first names
-        if (fullname.includes(" ")) {
-            firstname = fullname.substring(fullname[0], fullname.indexOf(" "));
-        } else {
-            firstname = fullname.substring(0);
-        }
+    const texts = fullname.split(" ");
 
-        // making middle names (if they have it)
-        if (fullname.includes(" ") && fullname.lastIndexOf(" ") != fullname.indexOf(" ") && !fullname.includes(`"`)) {
-            middlename = fullname.substring(fullname.indexOf(" ")+1, fullname.lastIndexOf(" "));
-        } else {
-            middlename = ""
-        }
-
-        // making last names (if they have it)
-        // hyphen cases
-        if (fullname.includes(" ")) {
-            lastname = fullname.substring(fullname.lastIndexOf(" ")+1);
-        } else {
-            lastname = "";
-        }
-
-        // making nicknames (if they have it)
-        if (fullname.includes(`"`)) {
-            nickname = fullname.substring(fullname.indexOf("\"")+1, fullname.lastIndexOf("\""));
-        } else {
-            nickname = "";
-        }
-
-        //adding images
-        if (lastname && !lastname.includes("-")) {
-            image = lastname.toLowerCase() + "_" + firstname[0].toLowerCase() + ".png";
-        } else if (lastname.includes("-")) {
-            let afterhyphen = lastname.substring(lastname.indexOf("-")+1);
-            image = afterhyphen.toLowerCase() + "_" + firstname[0].toLowerCase() + ".png";
-        }
-
-        console.log("first name:", firstname);
-        console.log("middle name:", middlename);
-        console.log("last name:", lastname);
-        console.log("nickname:", nickname);
-        console.log("house:", house);
-        console.log("gender:", gender);
-        console.log("image:", image)
-        console.log("");
-
-        });
-
-
-    // BUILDING A NEW OBJECT PROTOTYPE
-
-    const Student = {
-        firstname: "",
-        middlename: "",
-        lastname: "",
-        nickname: "",
-        gender: "",
-        house: "",
-        image: "",
+    // making first names and last names
+    if (fullname.includes(" ") && fullname.lastIndexOf(" ") === fullname.indexOf(" ")) {
+        firstname = texts[0];
+        lastname = texts[1]; 
+    } else {
+        firstname = texts[0];
     }
 
-    // create a new object from prototype
-    const student = Object.create(Student);
+    // making middle names (if they have it)
+    if (fullname.includes(" ") && fullname.lastIndexOf(" ") != fullname.indexOf(" ") && !fullname.includes(`"`)) {
+        firstname = texts[0];
+        middlename = texts[1];
+        lastname = texts[2];
+    } else {
+        middlename = "";
+    }
 
+    // making nicknames (if they have it)
+    if (fullname.includes(`"`)) {
+        firstname = texts[0];
+        nickname = texts[1];
+        lastname = texts[2];
+    } else {
+        nickname = "";
+    }
+
+    //adding images
+    if (lastname && !lastname.includes("-")) {
+        image = lastname.toLowerCase() + "_" + firstname[0].toLowerCase() + ".png";
+    } else if (lastname.includes("-")) {
+        let afterhyphen = lastname.substring(lastname.indexOf("-")+1);
+        image = afterhyphen.toLowerCase() + "_" + firstname[0].toLowerCase() + ".png";
+    }
+
+    return student;
+}
+
+function newData(student) {
     // set properties on the object to the variables
     student.firstname = firstname;
     student.middlename = middlename;
@@ -164,9 +155,34 @@ function prepareData() {
     student.house = house;
 
     // add the object to an array of data objects
-    newStudentArray = Array.from(student);
+    allStudents = Array.from(student);
+    console.log(allStudents);
+};
 
-    console.log(newStudentArray)
+// DISPLAYING LIST OF STUDENTS
+function displayList(students) {
+    // TODO clear the list
+    //document.querySelector("#list tbody").innerHTML = "";
 
+    // ! build a new list
+    //students.forEach(displayStudent);
 
+    // just for now
+    students.forEach(newData);
+}
+
+function displayStudent(student) {
+    // TODO create clone
+    // const clone = document.querySelector("template #student").content.cloneNode(true);
+    
+    // TODO set clone data
+    /* clone.querySelector("[data-field=firstname]").textContent = student.firstname;
+    clone.querySelector("[data-field=middlename]").textContent = student.middlename;
+    clone.querySelector("[data-field=lastname]").textContent = student.lastname;
+    clone.querySelector("[data-field=nickname]").textContent = student.nickname;
+    clone.querySelector("[data-field=gender]").textContent = student.gender;
+    clone.querySelector("[data-field=house]").textContent = student.house;
+ */
+    // TODO append clone to list
+    // document.querySelector("#list tbody").appendChild( clone );
 }
